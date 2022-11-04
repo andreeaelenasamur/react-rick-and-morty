038f32037de685124d226696901e53e5edba9814
Andreea <andelenasamur@gmail.com>
2022-11-04 13:43:37 +0100

    api configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect,useState } from 'react'
+
 function App() {
+
+  const [characters, setCharacters] = useState([])
+
+  useEffect( () => {
+    async function fetchData(){
+      const response = await fetch('https://rickandmortyapi.com/api/character')
+      const data = await response.json()
+      setCharacters(data.results);
+    }
+
+    fetchData()
+}, [])
+
   return (
-    <h1>Hello world</h1>
-  )
+    <div>
+      <h1>Rick and Morty</h1>
+      {
+        characters.map(character => {
+          return (
+            <div>
+              <h2>{character.name}</h2>
+              <img src={character.image} alt={character.name}/>
+            </div>
+          )
+        })
+      }
+    </div>
+    )
 }
 
 export default App
